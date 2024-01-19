89331e4f37803a077f887217b455ab5a30746331
itonse <govl3dnjs@gmail.com>
2024-01-19 13:58:37 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/ll/gooHaeYu/GooHaeYuApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gooHaeYu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooHaeYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooHaeYuApplication.class, args);
+	}
+
+}
