b9b56bd89f4be68be2deb358c04b426959f79559
itonse <govl3dnjs@gmail.com>
2024-01-20 08:17:53 +0900

    feat : BaseTimeEntity 추가
    - 엔티티의 생성 및 수정 시간을 자동으로 기록하기 위한 BaseTimeEntity 클래스 추가
    - JPA Auditing 활성화

---
+++ src/main/java/com/ll/gooHaeYu/global/jpa/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.ll.gooHaeYu.global.jpa;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
