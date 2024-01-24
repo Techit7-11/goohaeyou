af7f1dba7edfbdd2ed9be9b7e87a0e52e6b371f2
itonse <govl3dnjs@gmail.com>
2024-01-24 21:32:08 +0900

    style : JobPostController 코드 포맷 정리

--- src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/controller/JobPostController.java
+++ src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/controller/JobPostController.java
@@ -22,21 +22,25 @@ public class JobPostController {
     private final JobPostService jobPostService;
 
     @PostMapping
-    public ResponseEntity<String> writePost(Authentication authentication, @Valid @RequestBody JobPostForm.Register form) {
+    public ResponseEntity<String> writePost(Authentication authentication,
+                                            @Valid @RequestBody JobPostForm.Register form) {
         Long jobPostId = jobPostService.writePost(authentication.getName(), form);
 
         return ResponseEntity.created(URI.create("/api/job-posts" + jobPostId)).build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> modifyPost(Authentication authentication, @PathVariable(name = "id") Long id, @Valid @RequestBody JobPostForm.Modify request) {
+    public ResponseEntity<String> modifyPost(Authentication authentication,
+                                             @PathVariable(name = "id") Long id,
+                                             @Valid @RequestBody JobPostForm.Modify request) {
         jobPostService.modifyPost(authentication.getName(), id, request);
 
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deletePost(Authentication authentication, @PathVariable(name = "id") Long id) {
+    public ResponseEntity<String> deletePost(Authentication authentication,
+                                             @PathVariable(name = "id") Long id) {
         jobPostService.deletePost(authentication.getName(), id);
 
         return ResponseEntity.noContent().build();
