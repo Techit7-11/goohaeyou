b1e718a6ce818a083325eead68cda97d09aaefde
itonse <govl3dnjs@gmail.com>
2024-01-24 21:30:50 +0900

    refactor : 코드 포맷 정리 및 사용하지 않는 import 제거

--- src/main/java/com/ll/gooHaeYu/standard/base/utils/JwtUtil.java
+++ src/main/java/com/ll/gooHaeYu/standard/base/utils/JwtUtil.java
@@ -2,13 +2,10 @@ package com.ll.gooHaeYu.standard.base.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 
-import javax.crypto.SecretKey;
 import java.util.Date;
 
 public class JwtUtil {
-
 
     public static String createJwt(String username, String secretKey, Long expiredMs) {
         return Jwts.builder()
