164a918bdc0a09693cc3fb58deb29f6cad5cad5b
itonse <govl3dnjs@gmail.com>
2024-01-26 23:58:21 +0900

    refactor: ResponseItemRepository 제거

--- src/main/java/com/ll/gooHaeYu/domain/application/application/repository/ResponseItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ll.gooHaeYu.domain.application.application.repository;
-
-import com.ll.gooHaeYu.domain.application.application.entity.ResponseItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResponseItemRepository extends JpaRepository<ResponseItem, Long> {
-}
