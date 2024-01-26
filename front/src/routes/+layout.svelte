e454ae583b7d2d4f4093e55d3a9062301e7025d9
Jieun Hong <govl3dnjs@gmail.com>
2024-01-27 05:14:51 +0900

    Feature/front settings (#17)
    * feat: front settings
    
    * feat: 테일윈드 설치 및 설정파일 생성
    
    * design: 프리텐다드 폰트 적용
    
    * feat: 기본 페이지, 레이아웃 적용
    
    * feat: Jackson 직렬화 설정 수정
    
    * feat: application-prod.yml 추가
    
    - 운영모드 기본 환경 설정

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '$lib/app.css';
+
+    const { children } = $props();
+</script>
+
+<header>
+    <a href="/">홈</a>
+    <a href="/member/login">로그인</a>
+    <a href="/member/join">가입</a>
+</header>
+<main>{@render children()}</main>
+<footer>푸터</footer>
+  
