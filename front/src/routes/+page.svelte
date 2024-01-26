14fd325a6d80b403262c5359ae769e8386172c10
itonse <govl3dnjs@gmail.com>
2024-01-27 01:09:59 +0900

    design: 프리텐다드 폰트 적용

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    import "$lib/app.css"
+	import '$lib/app.css';
 </script>
 
-<h1 class = "text-blue-500">Welcome to SvelteKit</h1>
+<h1 class="text-blue-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
