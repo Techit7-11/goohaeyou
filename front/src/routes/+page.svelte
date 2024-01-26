f6274638d2e1a0544acbc943c540084df2f82078
itonse <govl3dnjs@gmail.com>
2024-01-27 00:46:27 +0900

    feat: 테일윈드 설치 및 설정파일 생성

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import "$lib/app.css"
+</script>
+
+<h1 class = "text-blue-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
