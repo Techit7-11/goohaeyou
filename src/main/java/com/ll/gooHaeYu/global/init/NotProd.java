b5a4b7fe409abc83cdb724a33612fc6962356174
itonse <govl3dnjs@gmail.com>
2024-01-28 01:52:35 +0900

    refactor: 샘플 데이터를 원할 때만 생성할 수 있도록 설정
    - application-dev.yml 에서 app: init-run: true 로 설정 하면, 샘플 데이터 생성

--- src/main/java/com/ll/gooHaeYu/global/init/NotProd.java
+++ src/main/java/com/ll/gooHaeYu/global/init/NotProd.java
@@ -8,12 +8,12 @@ import com.ll.gooHaeYu.domain.member.member.repository.MemberRepository;
 import com.ll.gooHaeYu.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.IntStream;
@@ -27,14 +27,17 @@ public class NotProd {
     private final JobPostService jobPostService;
     private final MemberRepository memberRepository;
 
+    @Value("${app.init-run}")
+    private boolean initRun;   // application-dev.yml 에서 app: init-run: true 로 설정 하면, 샘플 데이터 생성
+
     @Bean
-    @Order(2)
     public ApplicationRunner initNotProd() {
         return new ApplicationRunner() {
 
             @Transactional
             @Override
             public void run(ApplicationArguments args) {
+                if (!initRun) return;
                 if (memberRepository.findById(1L).isPresent()) return;
 
                 JoinForm joinForm = JoinForm.builder()
