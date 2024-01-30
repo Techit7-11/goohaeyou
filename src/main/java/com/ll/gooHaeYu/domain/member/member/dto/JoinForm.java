40e8be4c70a66ba89cdf5ca5edf43af7cedd891d
itonse <govl3dnjs@gmail.com>
2024-01-31 07:09:12 +0900

    refactor: 회원가입 폼 수정

--- src/main/java/com/ll/gooHaeYu/domain/member/member/dto/JoinForm.java
+++ src/main/java/com/ll/gooHaeYu/domain/member/member/dto/JoinForm.java
@@ -1,12 +1,16 @@
 package com.ll.gooHaeYu.domain.member.member.dto;
 
+import com.ll.gooHaeYu.domain.member.member.entity.type.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Getter
 @Builder
@@ -21,5 +25,28 @@ public class JoinForm {
     @NotBlank(message = "비밀번호는 필수 항목입니다.")
     @Size(min = 8, max = 20, message = "비밀번호는 최소 8자 ~ 최대 20자 이내로 입력해주세요.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 항목입니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "이름은 한글 또는 영문만 가능합니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수 항목입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "휴대폰 번호는 필수 항목입니다.")
+    @Pattern(regexp = "^\\d{10,11}$", message = "휴대폰 번호는 숫자만 10자리 또는 11자리여야 합니다.")
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @NotBlank(message = "지역을 입력해주세요.")
+    private String location;
+
+    @NotBlank(message = "생년월일은 필수 입력 항목입니다.")
+    @Past(message = "생년월일은 현재보다 앞선 날짜여야 합니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birth;
 
 }
