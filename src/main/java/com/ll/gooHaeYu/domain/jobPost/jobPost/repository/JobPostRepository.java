1c0fbf238f46fe1ff241b5d5c3bbcc8e5e8484a7
Yimin2 <gownfla14@naver.com>
2024-02-05 14:51:10 +0900

    refactor:  미사용 import 제거

--- src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/repository/JobPostRepository.java
+++ src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/repository/JobPostRepository.java
@@ -1,10 +1,7 @@
 package com.ll.gooHaeYu.domain.jobPost.jobPost.repository;
 
 import com.ll.gooHaeYu.domain.jobPost.jobPost.entity.JobPost;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
