80928089936479c345f1992924a4e5cb2ecee895
Yimin2 <gownfla14@naver.com>
2024-02-06 16:23:04 +0900

    feat:  tailwind & daysiUi 적용

--- front/src/routes/member/me/+page.svelte
+++ front/src/routes/member/me/+page.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
-  import rq from '$lib/rq/rq.svelte';
-  import prettyDate from '$lib/utils/ut';
+	import rq from '$lib/rq/rq.svelte';
+	import prettyDate from '$lib/utils/ut';
 </script>
 
-<div class="flex-grow flex justify-center items-center">
-  <div class="w-full max-w-sm px-2">
-    <div class="text-center">
-      <div class="font-bold text-lg">내 정보</div>
-      <div class="mt-3 text-gray-400">현재 로그인한 회원의 정보입니다.</div>
-    </div>
-
-    <div class="divider"></div>
-
-    <div class="grid grid-colos-1 gap-4">
-      <div class="flex justify-center">
-      </div>
-
-      <div>
-        <div class="font-bold">아이디</div>
-        <div class="text-gray-500 mt-1">{rq.member.username}</div>
-      </div>
-
-      <div>
-        <div class="font-bold">이름</div>
-        <div class="text-gray-500 mt-1">{rq.member.name}</div>
-      </div>
-    </div>
-  </div>
+<div class="flex items-center justify-center min-h-screen bg-base-100">
+	<div class="container mx-auto px-4">
+		<div class="max-w-sm mx-auto my-10">
+			<div class="text-center">
+				<div class="font-bold text-xl">내 정보</div>
+				<div class="mt-3 text-gray-500">현재 로그인한 회원의 정보입니다.</div>
+			</div>
+			<div class="divider"></div>
+			<div class="grid grid-colos-1 gap-4">
+				<div class="flex justify-between items-center w-full">
+					<div class="font-bold">아이디</div>
+					<div class="text-gray-500 flex-auto text-right">{rq.member.username}</div>
+				</div>
+				<div class="flex justify-between items-center w-full">
+					<div class="font-bold">이름</div>
+					<div class="text-gray-500 flex-auto text-right">{rq.member.name}</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
