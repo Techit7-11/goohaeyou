676edd226d7d19b9b5576eb3a3b4b3b00fc73774
itonse <govl3dnjs@gmail.com>
2024-02-09 10:21:29 +0900

    chore: 자동 배포 설정
    - 자동 배포 브랜치를 release로 설정

--- front/src/lib/types/api/v1/schema.d.ts
+++ front/src/lib/types/api/v1/schema.d.ts
@@ -298,8 +298,8 @@ export interface components {
       pageNumber?: number;
       /** Format: int32 */
       pageSize?: number;
-      paged?: boolean;
       unpaged?: boolean;
+      paged?: boolean;
     };
     RsDataPageJobPostDto: {
       resultCode?: string;
