c867a407b6fc2f6f564eda33641645afd862ba92
itonse <govl3dnjs@gmail.com>
2024-02-10 20:57:14 +0900

    fix: 테스트 클래스에 test 프로필 활성화

--- src/test/java/com/ll/gooHaeYu/GooHaeYuApplicationTests.java
+++ src/test/java/com/ll/gooHaeYu/GooHaeYuApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.gooHaeYu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GooHaeYuApplicationTests {
 
 	@Test
