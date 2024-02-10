fbd643384e0873e252261f6c0cc4509d8833e528
itonse <govl3dnjs@gmail.com>
2024-02-11 04:01:32 +0900

    test: 인증 되지 않은 사용자는 자동으로 구글 로그인 페이지로 이동하는 문제 해결 시도3

--- src/main/java/com/ll/gooHaeYu/global/config/SecurityConfig.java
+++ src/main/java/com/ll/gooHaeYu/global/config/SecurityConfig.java
@@ -50,13 +50,15 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests(requests -> {
                     requests
-                            .requestMatchers("/h2-console/**").permitAll()                            .requestMatchers("/api/member/socialLogin/**").permitAll()
+                            .requestMatchers("/h2-console/**").permitAll()
+                            .requestMatchers("/api/member/socialLogin/**").permitAll()
                             .requestMatchers("/oauth2/authorization/**").permitAll() // OAuth 2.0 인증 엔드포인트에 대한 접근 허용
                             .requestMatchers("/login", "/api/member/join", "api/member/login", "api/member/logout").permitAll()
                             .requestMatchers(HttpMethod.GET, "/api/job-posts/{id:\\d+}", "/api/job-posts",
                                     "/api/job-posts/search",
                                     "/api/post-comment/{postId}").permitAll()
-                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll();
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                            .requestMatchers("/**").permitAll();
 //                            .anyRequest()
 //                            .authenticated();
 
