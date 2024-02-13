1f375a3a394a1b149f45c34e331e72c52db891dc
itonse <govl3dnjs@gmail.com>
2024-02-14 05:45:08 +0900

    fix: SecurityConfig 수정
    - 개발 환경에서는 H2 DB 콘솔에 접근할 수 있도록 설정 (운영 환경에서는 제거 필요)

--- front/src/lib/types/api/v1/schema.d.ts
+++ front/src/lib/types/api/v1/schema.d.ts
@@ -15,16 +15,6 @@ export interface paths {
     /** 알림 읽음 처리 */
     put: operations["read"];
   };
-  "/api/member": {
-    /** 내 정보 조회 */
-    get: operations["detailMember"];
-    /** 내 정보 수정 */
-    put: operations["modifyMember"];
-  };
-  "/api/member/social": {
-    /** 최초 소셜로그인 - 필수 회원정보 입력 */
-    put: operations["updateSocialMember"];
-  };
   "/api/job-posts/{id}": {
     /** 구인공고 단건 조회 */
     get: operations["showDetailPost"];
@@ -33,31 +23,9 @@ export interface paths {
     /** 구인공고 삭제 */
     delete: operations["deleteJobPost"];
   };
-  "/api/applications/{id}": {
-    /** 지원서 상세 내용 */
-    get: operations["detailApplication"];
-    /** 지원서 수정 */
-    put: operations["modifyApplication"];
-    /** 지원서 작성 */
-    post: operations["writeApplication"];
-    /** 지원서 삭제 */
-    delete: operations["deleteApplication"];
-  };
   "/api/post-comment/{postId}/comment": {
     /** 댓글 작성 */
     post: operations["write"];
-  };
-  "/api/member/logout": {
-    /** 로그아웃 처리 및 쿠키 삭제 */
-    post: operations["logout"];
-  };
-  "/api/member/login": {
-    /** 로그인, accessToken, refreshToken 쿠키 생성됨 */
-    post: operations["login"];
-  };
-  "/api/member/join": {
-    /** 회원가입 */
-    post: operations["join"];
   };
   "/api/job-posts": {
     /** 구인공고 글 목록 가져오기 */
@@ -86,22 +54,6 @@ export interface paths {
   "/api/notification": {
     /** 유저 별 알림리스트 */
     get: operations["getList"];
-  };
-  "/api/member/myposts": {
-    /** 내 공고 조회 */
-    get: operations["detailMyPosts"];
-  };
-  "/api/member/myinterest": {
-    /** 내 관심 공고 조회 */
-    get: operations["detailMyInterestingPosts"];
-  };
-  "/api/member/mycomments": {
-    /** 내 댓글 조회 */
-    get: operations["detailMyComments"];
-  };
-  "/api/member/myapplications": {
-    /** 내 지원서 조회 */
-    get: operations["detailMyApplications"];
   };
   "/api/job-posts/sort": {
     /** 구인공고 글 목록 정렬 */
@@ -136,40 +88,15 @@ export interface components {
       content: string;
     };
     Modify: {
-      /** @enum {string} */
-      gender?: "MALE" | "FEMALE" | "UNDEFINED";
-      location?: string;
-      /** Format: date */
-      birth?: string;
-      password?: string;
-    };
-    SocialProfileForm: {
-      name: string;
-      phoneNumber: string;
-      /** @enum {string} */
-      gender?: "MALE" | "FEMALE" | "UNDEFINED";
+      title: string;
+      body: string;
       location: string;
-      /** Format: date */
-      birth: string;
-    };
-    MemberDto: {
-      /** Format: int64 */
-      id: number;
-      username: string;
+      /** Format: int32 */
+      minAge?: number;
       /** @enum {string} */
       gender?: "MALE" | "FEMALE" | "UNDEFINED";
-      location?: string;
       /** Format: date */
-      birth?: string;
-      name?: string;
-      phoneNumber?: string;
-    };
-    RsDataMemberDto: {
-      resultCode?: string;
-      /** Format: int32 */
-      statusCode?: number;
-      msg?: string;
-      data?: components["schemas"]["MemberDto"];
+      deadLine: string;
     };
     RsDataModify: {
       resultCode?: string;
@@ -184,36 +111,6 @@ export interface components {
       statusCode?: number;
       msg?: string;
       data?: components["schemas"]["Register"];
-    };
-    LoginForm: {
-      username: string;
-      password: string;
-    };
-    JoinForm: {
-      username: string;
-      password: string;
-      name: string;
-      phoneNumber: string;
-      /** @enum {string} */
-      gender?: "MALE" | "FEMALE" | "UNDEFINED";
-      location: string;
-      /** Format: date */
-      birth: string;
-    };
-    RsDataJoinForm: {
-      resultCode?: string;
-      /** Format: int32 */
-      statusCode?: number;
-      msg?: string;
-      data?: components["schemas"]["JoinForm"];
-    };
-    RsDataURI: {
-      resultCode?: string;
-      /** Format: int32 */
-      statusCode?: number;
-      msg?: string;
-      /** Format: uri */
-      data?: string;
     };
     CommentDto: {
       /** Format: int64 */
@@ -277,32 +174,6 @@ export interface components {
       msg?: string;
       data?: components["schemas"]["JobPostDto"][];
     };
-    ApplicationDto: {
-      /** Format: int64 */
-      id: number;
-      /** Format: int64 */
-      jobPostId: number;
-      jobPostAuthorUsername: string;
-      jobPostName: string;
-      author: string;
-      /** Format: int64 */
-      postId: number;
-      body: string;
-      /** Format: date */
-      birth: string;
-      phone: string;
-      location: string;
-      /** Format: date-time */
-      createdAt?: string;
-      approve?: boolean;
-    };
-    RsDataListApplicationDto: {
-      resultCode?: string;
-      /** Format: int32 */
-      statusCode?: number;
-      msg?: string;
-      data?: components["schemas"]["ApplicationDto"][];
-    };
     JobPostDetailDto: {
       /** Format: int64 */
       id: number;
@@ -357,12 +228,31 @@ export interface components {
       msg?: string;
       data?: components["schemas"]["GetPostsResponseBody"];
     };
-    RsDataApplicationDto: {
+    ApplicationDto: {
+      /** Format: int64 */
+      id: number;
+      /** Format: int64 */
+      jobPostId: number;
+      jobPostAuthorUsername: string;
+      jobPostName: string;
+      author: string;
+      /** Format: int64 */
+      postId: number;
+      body: string;
+      /** Format: date */
+      birth: string;
+      phone: string;
+      location: string;
+      /** Format: date-time */
+      createdAt?: string;
+      approve?: boolean;
+    };
+    RsDataListApplicationDto: {
       resultCode?: string;
       /** Format: int32 */
       statusCode?: number;
       msg?: string;
-      data?: components["schemas"]["ApplicationDto"];
+      data?: components["schemas"]["ApplicationDto"][];
     };
   };
   responses: never;
@@ -427,47 +317,6 @@ export interface operations {
       };
     };
   };
-  /** 내 정보 조회 */
-  detailMember: {
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataMemberDto"];
-        };
-      };
-    };
-  };
-  /** 내 정보 수정 */
-  modifyMember: {
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["Modify"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: never;
-      };
-    };
-  };
-  /** 최초 소셜로그인 - 필수 회원정보 입력 */
-  updateSocialMember: {
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["SocialProfileForm"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataMemberDto"];
-        };
-      };
-    };
-  };
   /** 구인공고 단건 조회 */
   showDetailPost: {
     parameters: {
@@ -519,76 +368,6 @@ export interface operations {
       };
     };
   };
-  /** 지원서 상세 내용 */
-  detailApplication: {
-    parameters: {
-      path: {
-        id: number;
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataApplicationDto"];
-        };
-      };
-    };
-  };
-  /** 지원서 수정 */
-  modifyApplication: {
-    parameters: {
-      path: {
-        id: number;
-      };
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["Modify"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: never;
-      };
-    };
-  };
-  /** 지원서 작성 */
-  writeApplication: {
-    parameters: {
-      path: {
-        id: number;
-      };
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["Register"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataURI"];
-        };
-      };
-    };
-  };
-  /** 지원서 삭제 */
-  deleteApplication: {
-    parameters: {
-      path: {
-        id: number;
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: never;
-      };
-    };
-  };
   /** 댓글 작성 */
   write: {
     parameters: {
@@ -606,49 +385,6 @@ export interface operations {
       200: {
         content: {
           "*/*": components["schemas"]["RsDataRegister"];
-        };
-      };
-    };
-  };
-  /** 로그아웃 처리 및 쿠키 삭제 */
-  logout: {
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": Record<string, never>;
-        };
-      };
-    };
-  };
-  /** 로그인, accessToken, refreshToken 쿠키 생성됨 */
-  login: {
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["LoginForm"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataMemberDto"];
-        };
-      };
-    };
-  };
-  /** 회원가입 */
-  join: {
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["JoinForm"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataJoinForm"];
         };
       };
     };
@@ -769,50 +505,6 @@ export interface operations {
       };
     };
   };
-  /** 내 공고 조회 */
-  detailMyPosts: {
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataListJobPostDto"];
-        };
-      };
-    };
-  };
-  /** 내 관심 공고 조회 */
-  detailMyInterestingPosts: {
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataListJobPostDto"];
-        };
-      };
-    };
-  };
-  /** 내 댓글 조회 */
-  detailMyComments: {
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataListCommentDto"];
-        };
-      };
-    };
-  };
-  /** 내 지원서 조회 */
-  detailMyApplications: {
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "*/*": components["schemas"]["RsDataListApplicationDto"];
-        };
-      };
-    };
-  };
   /** 구인공고 글 목록 정렬 */
   findAllPostSort: {
     parameters: {
@@ -844,7 +536,7 @@ export interface operations {
       /** @description OK */
       200: {
         content: {
-          "*/*": components["schemas"]["JobPostDto"][];
+          "*/*": components["schemas"]["RsDataListJobPostDto"];
         };
       };
     };
