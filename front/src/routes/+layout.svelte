e426fd36907e9b3300ca6c3a18d836fd2dc243c9
Yimin2 <gownfla14@naver.com>
2024-02-14 11:56:37 +0900

    design:  nav 레이아웃 변경

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -22,6 +22,9 @@
               function NavAlert() {
                window.location.href = '/notification/list';
                   }
+                  function NavSearch() {
+                                 window.location.href = '/job-post/search';
+                                    }
 
 	onMount(() => {
 		rq.initAuth();
@@ -34,12 +37,6 @@
 			<a class="btn btn-ghost text-xl" href="/">GooHaeYou</a>
 		</div>
 		<div class="navbar-center">
-     <div class="form-control">
-          <input type="text" placeholder="Search" class="input w-24 md:w-auto text-center" />
-        </div>
-<button class="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    </button>
 		</div>
 		<div class="navbar-end">
 			{#if rq.isLogout()}
@@ -108,13 +105,17 @@
 	</aside>
 </footer>
 <div class="btm-nav">
+<button on:click={NavHomepage}>
+    <span class="btm-nav-label"></span>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+  </button>
+   <button on:click={NavSearch}>
+    <span class="btm-nav-label"></span>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+  </button>
    <button on:click={NavAlert}>
     <span class="btm-nav-label"></span>
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-  </button>
-   <button on:click={NavHomepage}>
-    <span class="btm-nav-label"></span>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
   </button>
    <button on:click={NavMyPage}>
       <span class="btm-nav-label"></span>
