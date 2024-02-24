c0652a24ca6d8e176d33cd5f7863cda277c81a11
itonse <govl3dnjs@gmail.com>
2024-02-25 07:29:19 +0900

    test: prod 소셜로그인 테스트2

--- src/main/java/com/ll/gooHaeYu/domain/member/member/dto/JoinForm.java
+++ src/main/java/com/ll/gooHaeYu/domain/member/member/dto/JoinForm.java
@@ -19,7 +19,7 @@ import java.time.LocalDate;
 public class JoinForm {
 
     @NotBlank(message = "아이디는 필수 항목입니다.")
-    @Size(min = 4, max = 20, message = "아이디는 최소 4자 ~ 최대 20자 이내로 입력해주세요.")
+    @Size(min = 4, max = 20, message = "아이디는 최소 8자 ~ 최대 20자 이내로 입력해주세요.")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 항목입니다.")
