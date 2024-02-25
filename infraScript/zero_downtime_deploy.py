5bd14407fcd4669840b5276d0a2464843ad9df64
itonse <govl3dnjs@gmail.com>
2024-02-26 07:20:04 +0900

    style: 파이썬 스크립트 들여쓰기 수정
    - NameError: name 'self' is not defined 오류 방지

--- infraScript/zero_downtime_deploy.py
+++ infraScript/zero_downtime_deploy.py
@@ -6,7 +6,6 @@ import subprocess
 import time
 from typing import Dict, Optional
 
-
 class ServiceManager:
     # 초기화 함수
     def __init__(self, socat_port: int = 8080, sleep_duration: int = 3) -> None:
@@ -48,8 +47,8 @@ class ServiceManager:
         os.system(
             f"docker run --name={name} -p {port}:8080 -v /docker_projects/goohaeyou/volumes/gen:/gen --restart unless-stopped -e TZ=Asia/Seoul --pull always -d ghcr.io/techit7-11/goohaeyou")
 
+    # Socat 포트를 전환하는 함수
     def _switch_port(self) -> None:
-        # Socat 포트를 전환하는 함수
         cmd: str = f"ps aux | grep 'socat -t0 TCP-LISTEN:{self.socat_port}' | grep -v grep | awk '{{print $2}}'"
         pid: str = subprocess.getoutput(cmd)
 
@@ -59,17 +58,15 @@ class ServiceManager:
         os.system(
             f"nohup socat -t0 TCP-LISTEN:{self.socat_port},fork,reuseaddr TCP:localhost:{self.next_port} &>/dev/null &")
 
-
     # 서비스 상태를 확인하는 함수
     def _is_service_up(self, port: int) -> bool:
         url = f"http://127.0.0.1:{port}/actuator/health"
         try:
-            response = requests.get(url, timeout=5)  # 5초 이내 응답 없으면 예외 발생
+            response = requests.get(url, timeout=5)
             if response.status_code == 200 and response.json().get('status') == 'UP':
                 return True
         except requests.RequestException:
-            pass
-        return False
+            return False
 
     def _is_service_ready(self, port: int) -> bool:
         ready_url = f"http://127.0.0.1:{port}/ready"
@@ -79,34 +76,33 @@ class ServiceManager:
                 if response.status_code == 200:
                     return True
             except requests.RequestException:
-                pass
-            time.sleep(5)
+                time.sleep(5)
         return False
 
     # 서비스를 업데이트하는 함수
     def update_service(self) -> None:
         self._find_current_service()
-    self._find_next_service()
+        self._find_next_service()
 
-    self._remove_container(self.next_name)
-    self._run_container(self.next_name, self.next_port)
+        self._remove_container(self.next_name)
+        self._run_container(self.next_name, self.next_port)
 
-    # 새 서비스가 'UP' 상태가 될 때까지 기다림
-    while not self._is_service_up(self.next_port):
-        print(f"Waiting for {self.next_name} to be 'UP'...")
-        time.sleep(self.sleep_duration)
+        # 새 서비스가 'UP' 상태가 될 때까지 기다림
+        while not self._is_service_up(self.next_port):
+            print(f"Waiting for {self.next_name} to be 'UP'...")
+            time.sleep(self.sleep_duration)
 
-    # 새 서비스가 완전히 준비될 때까지 추가로 기다림
-    if self._is_service_ready(self.next_port):
-        print(f"{self.next_name} is now ready.")
-        self._switch_port()
+        # 새 서비스가 완전히 준비될 때까지 추가로 기다림
+        if self._is_service_ready(self.next_port):
+            print(f"{self.next_name} is now ready.")
+            self._switch_port()
 
-        if self.current_name is not None:
-            self._remove_container(self.current_name)
+            if self.current_name is not None:
+                self._remove_container(self.current_name)
 
-        print("Switched service successfully!")
-    else:
-        print(f"Failed to switch to {self.next_name}: Service not ready.")
+            print("Switched service successfully!")
+        else:
+            print(f"Failed to switch to {self.next_name}: Service not ready.")
 
 
 if __name__ == "__main__":
