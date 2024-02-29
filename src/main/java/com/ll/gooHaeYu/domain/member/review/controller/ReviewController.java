21936ce46aab3a3f729870aec64cb4c3db0b4e05
Yimin2 <gownfla14@naver.com>
2024-02-29 10:26:46 +0900

    feat:  임시 저장

--- src/main/java/com/ll/gooHaeYu/domain/member/review/controller/ReviewController.java
+++ src/main/java/com/ll/gooHaeYu/domain/member/review/controller/ReviewController.java
@@ -1,7 +1,28 @@
 package com.ll.gooHaeYu.domain.member.review.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ll.gooHaeYu.domain.member.review.entity.Review;
+import com.ll.gooHaeYu.domain.member.review.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Review review) {
+        Review createdReview = reviewService.createReview(review);
+        return new ResponseEntity<>(createdReview, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/jobAds/{jobAdId}")
+    public ResponseEntity<List<Review>> getReviewsByJobAdId(@PathVariable Long jobAdId) {
+        List<Review> reviews = reviewService.getReviewsByJobAdId(jobAdId);
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
 }
