8451cf17bff999df10aa42cbba32fa36c37f913e
itonse <govl3dnjs@gmail.com>
2024-03-05 22:09:32 +0900

    feat: Payment 엔티티 추가
    - Member와 다대일 단방향 연관관계
    - status(결제상태) 필드는 추후 작업 예정

---
+++ src/main/java/com/ll/gooHaeYu/domain/payment/payment/entity/Payment.java
@@ -0,0 +1,94 @@
+package com.ll.gooHaeYu.domain.payment.payment.entity;
+
+import com.ll.gooHaeYu.domain.member.member.entity.Member;
+import com.ll.gooHaeYu.domain.payment.payment.dto.PaymentResDto;
+import com.ll.gooHaeYu.domain.payment.payment.entity.type.PayType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Builder
+@Getter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private String paymentKey;
+
+    private Long totalAmount;
+
+    private String payType;
+
+    private String status;
+
+    private String approvedAt;
+
+    private String orderId;
+
+    private String orderName;
+
+    private boolean paid;
+
+    private boolean canceled;
+
+    private String cancelReason;
+
+    private String failReason;
+
+    private Long applicationId;
+
+    public void updatePayer(Member member) {
+        this.member = member;
+    }
+
+    public void updatePayType(PayType payType) {
+        this.payType = payType.getDescription();
+    }
+
+    public void updatePaymentKey(String paymentKey) {
+        this.paymentKey = paymentKey;
+    }
+
+    public void markAsPaid() {
+        this.paid = true;
+    }
+
+    public void markAsUnpaid() {
+        this.paid = false;
+    }
+
+    public void recordFailReason(String failReason) {
+        this.failReason = failReason;
+    }
+
+    public void recordApprovedAt(String localDateTime) {
+        this.approvedAt = localDateTime;
+    }
+
+    public PaymentResDto toPaymentRespDto() {
+        PayType payTypeEnum = PayType.findByDescription(payType);
+
+        return PaymentResDto.builder()
+                .payType(payTypeEnum)
+                .amount(totalAmount)
+                .orderId(orderId)
+                .orderName(orderName)
+                .payer(member.getUsername())
+                .canceled(canceled)
+                .failReason(failReason)
+                .build();
+    }
+}
