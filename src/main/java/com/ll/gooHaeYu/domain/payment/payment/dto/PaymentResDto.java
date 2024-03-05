2d5edb3d75ba7bb981539979003ed99203da92f1
itonse <govl3dnjs@gmail.com>
2024-03-05 22:11:43 +0900

    feat: 결제 요청, 응답 DTO 생성
    - 요청 DTO는 프론트 페이지에서 값 입력

---
+++ src/main/java/com/ll/gooHaeYu/domain/payment/payment/dto/PaymentResDto.java
@@ -0,0 +1,39 @@
+package com.ll.gooHaeYu.domain.payment.payment.dto;
+
+import com.ll.gooHaeYu.domain.payment.payment.entity.type.PayType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class PaymentResDto {
+    @NotNull
+    private PayType payType;
+
+    @NotNull
+    private Long amount;
+
+    @NotBlank
+    private String orderId;
+
+    @NotBlank
+    private String orderName;
+
+    @NotBlank
+    private String payer;
+
+    private String successUrl;
+
+    private String failUrl;
+
+    private String failReason;
+
+    private boolean canceled;
+
+    private String cancelReason;
+
+}
