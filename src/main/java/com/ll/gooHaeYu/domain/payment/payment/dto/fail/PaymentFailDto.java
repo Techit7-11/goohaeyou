3d0741eed37bf8413eafbe9469629d76733dace2
itonse <govl3dnjs@gmail.com>
2024-03-05 22:13:34 +0900

    feat: 결제 성공, 실패 DTO 생성

---
+++ src/main/java/com/ll/gooHaeYu/domain/payment/payment/dto/fail/PaymentFailDto.java
@@ -0,0 +1,12 @@
+package com.ll.gooHaeYu.domain.payment.payment.dto.fail;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PaymentFailDto {
+    private String errorCode;
+    private String errorMessage;
+    private String orderId;
+}
