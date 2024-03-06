fe7e3f765286146b853c83a0fe36a149ae228563
Jieun Hong <govl3dnjs@gmail.com>
2024-03-07 07:45:01 +0900

    Hotfix/front bug #115 PR 충돌 해결 (#117)
    * fix: 공고상세 페이지 충돌 해결
    
    * fix: 지원서 상세 페이지 충돌 해결
    
    * fix: 지원서 목록 페이지 충돌 해결
    
    * fix: 지원서 수정 페이지 복구

--- front/src/routes/applications/detail/[id]/+page.svelte
+++ front/src/routes/applications/detail/[id]/+page.svelte
@@ -65,46 +65,27 @@
 </script>
 
 {#await loadApplication() then application}
-	<div class="container mx-auto p-4">
-		<div class="card bg-base-100 shadow-xl rounded-lg p-5">
-			<div class="flex items-center mb-4">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6 mr-2"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-					/>
-				</svg>
-				<h2 class="card-title text-xl font-bold">지원서 상세 정보</h2>
-			</div>
-
-			<div class="bg-white rounded-md mb-4">
-				<div class="px-4 py-2 border-b border-gray-200">
-					<h3 class="text-lg font-semibold mb-2">지원자 정보</h3>
-				</div>
-
-				<div class="px-4 py-2 space-y-2">
-					<p><strong>아이디:</strong> {application.author}</p>
-					<p><strong>이름:</strong> {application.name}</p>
-					<p><strong>나이:</strong> {calculateAge(application.birth)}</p>
-					<p><strong>연락처:</strong> {application.phone}</p>
-					<p><strong>주소:</strong> {application.location}</p>
-				</div>
-			</div>
-			<hr class="my-6 -mt-3 border-t border-gray-300 opacity-50" />
-			<div class="mb-2">
-				<strong>작성 내용</strong>
-				<div class="p-3 bg-gray-100 rounded overflow-auto mt-1" style="max-height: 200px;">
-					{application.body}
-				</div>
-			</div>
+	<div class="flex justify-center items-center min-h-screen bg-base-100">
+		<div class="container mx-auto px-4 mt-5">
+			<div class="w-full max-w-4xl mx-auto">
+				<div class="card bg-base-100 shadow-xl rounded-lg p-5">
+					<div class="flex items-center mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6 mr-2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+							/>
+						</svg>
+						<h2 class="card-title text-xl font-bold">지원서 상세 정보</h2>
+					</div>
 
 					<div class="bg-white rounded-md mb-4">
 						<div class="px-4 py-2 border-b border-gray-200">
@@ -173,7 +154,7 @@
 						</div>
 					{/if}
 				</div>
-			{/if}
+			</div>
 
 			{#if application.jobPostAuthorUsername == rq.member.username && application.approve}
 				{#if application.depositStatus == '지원서 승인'}
@@ -189,7 +170,6 @@
 					>
 				{/if}
 			{/if}
-
 		</div>
 	</div>
 {:catch error}
