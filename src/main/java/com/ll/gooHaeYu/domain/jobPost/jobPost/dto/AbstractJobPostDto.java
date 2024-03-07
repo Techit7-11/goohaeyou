ec5b6d587d6764ab02e3f3fc513fadb9c8a7b432
itonse <govl3dnjs@gmail.com>
2024-03-08 08:00:39 +0900

    refactor: jobPost 관련 dto 수정

--- src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/dto/AbstractJobPostDto.java
+++ src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/dto/AbstractJobPostDto.java
@@ -1,6 +1,5 @@
 package com.ll.gooHaeYu.domain.jobPost.jobPost.dto;
 
-import com.ll.gooHaeYu.domain.jobPost.jobPost.entity.type.WageType;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
@@ -26,12 +25,6 @@ public class AbstractJobPostDto {
     private long interestsCount;
     @NotNull
     private String createdAt;
-    @NotNull
-    private String workTime;
-    @NotNull
-    private int cost;
-    @NotNull
-    private WageType wageType;
 
     private boolean employed;
     private boolean isClosed;
