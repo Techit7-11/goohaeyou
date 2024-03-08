886dbfa53c99aa205101c60d72d5de5d2a27bffa
jae9380 <ljy5314@gmail.com>
2024-03-08 14:48:29 +0900

    feat: batch test

---
+++ src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/CalculateService.java
@@ -0,0 +1,55 @@
+package com.ll.gooHaeYu.domain.calculate.calculate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class CalculateService {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager ptm;
+
+    @Bean
+    public Job batchJob1() {
+        return new JobBuilder("batchJob1", jobRepository)
+                .start(step1())
+                .next(step2())
+                .build();
+
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                },ptm)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                }, ptm)
+                .build();
+    }
+}
