e9fa51b3ade88224026da71505d4bdad9d5a6414
itonse <govl3dnjs@gmail.com>
2024-03-09 16:39:42 +0900

    fix: deploy.aws.yml 수정

--- front/src/lib/types/api/v1/schema.d.ts
+++ front/src/lib/types/api/v1/schema.d.ts
@@ -451,12 +451,7 @@ export interface components {
       /** Format: int64 */
       interestsCount: number;
       createdAt: string;
-      workTime: string;
-      /** Format: int32 */
-      cost: number;
-      /** @enum {string} */
-      wageType: "UNDEFINED" | "HOURLY" | "SALARY" | "PER_PROJECT";
-      employed?: boolean;
+      employed: boolean;
       /** Format: date */
       deadLine?: string;
       closed?: boolean;
@@ -511,12 +506,7 @@ export interface components {
       /** Format: int64 */
       interestsCount: number;
       createdAt: string;
-      workTime: string;
-      /** Format: int32 */
-      cost: number;
-      /** @enum {string} */
-      wageType: "UNDEFINED" | "HOURLY" | "SALARY" | "PER_PROJECT";
-      employed?: boolean;
+      employed: boolean;
       /** Format: date */
       deadLine?: string;
       body: string;
@@ -530,6 +520,11 @@ export interface components {
       interestedUsernames?: string[];
       /** Format: int32 */
       deposit?: number;
+      workTime?: string;
+      /** Format: int32 */
+      cost?: number;
+      /** @enum {string} */
+      wageType?: "UNDEFINED" | "HOURLY" | "SALARY" | "PER_PROJECT";
       closed?: boolean;
     };
     RsDataJobPostDetailDto: {
