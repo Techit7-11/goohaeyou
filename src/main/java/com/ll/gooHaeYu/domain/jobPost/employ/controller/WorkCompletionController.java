7e7c25fac44a00911d89cd322a46a35361c21e19
itonse <govl3dnjs@gmail.com>
2024-03-10 21:57:58 +0900

    docs: API 문서 수정

--- src/main/java/com/ll/gooHaeYu/domain/jobPost/employ/controller/WorkCompletionController.java
+++ src/main/java/com/ll/gooHaeYu/domain/jobPost/employ/controller/WorkCompletionController.java
@@ -3,12 +3,13 @@ package com.ll.gooHaeYu.domain.jobPost.employ.controller;
 import com.ll.gooHaeYu.domain.jobPost.employ.service.WorkCompletionService;
 import com.ll.gooHaeYu.global.rsData.RsData;
 import com.ll.gooHaeYu.global.security.MemberDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "WorkCompletion", description = "알바 완료 처리 API")
+@Tag(name = "WorkCompletion", description = "알바완료 처리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/jobs/complete")
@@ -16,6 +17,7 @@ public class WorkCompletionController {
     private final WorkCompletionService workCompletionService;
 
     @PatchMapping ("/{applicationId}/manually")
+    @Operation(summary = "구인자가 수동으로 알바완료 처리")
     public RsData<Void> completeJobManually(@AuthenticationPrincipal MemberDetails memberDetails,
                                          @PathVariable (name = "applicationId") Long applicationId) {
 
