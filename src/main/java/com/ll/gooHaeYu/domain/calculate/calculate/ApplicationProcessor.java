1556e362413a7fdfd9ae4ab6798e83b30a1ce60c
jae9380 <ljy5314@gmail.com>
2024-03-11 17:38:19 +0900

    feat: Initial calculate

--- src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/ApplicationProcessor.java
+++ src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/ApplicationProcessor.java
@@ -3,6 +3,8 @@ package com.ll.gooHaeYu.domain.calculate.calculate;
 import com.ll.gooHaeYu.domain.application.application.entity.Application;
 import com.ll.gooHaeYu.domain.jobPost.jobPost.entity.Wage;
 import com.ll.gooHaeYu.domain.jobPost.jobPost.repository.WageRepository;
+import com.ll.gooHaeYu.domain.member.member.entity.Member;
+import com.ll.gooHaeYu.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
@@ -11,23 +13,15 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Component
-@Transactional
 @RequiredArgsConstructor
 public class ApplicationProcessor implements ItemProcessor<Application, Application> {
 
     private final WageRepository wageRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public Application process(Application application) throws Exception {
-        System.out.println("------BEFORE------");
-        System.out.println("Application.ID : " + application.getId());
-        System.out.println("Application.DEPOSIT : " + application.getDepositStatus());
-        System.out.println("Application.EARN : " + application.getEarn());
-        System.out.println("Application.JOB_COMPLETED : " + application.getJobCompleted());
-        System.out.println("Application.RECEIVE : " + application.isReceive());
-        System.out.println("------BEFORE------");
-
-
+        System.out.println("------PROCESS------");
 
         // Application에서 jobPostDetail 필드를 이용하여 WAGE 테이블에서 해당 jobPostDetail에 대한 데이터 추출
         Wage wage = wageRepository.findByJobPostDetail(application.getJobPostDetail());
@@ -37,16 +31,18 @@ public class ApplicationProcessor implements ItemProcessor<Application, Applicat
             return null; // 처리할 데이터 없음
         }
 
+        Member member = application.getMember();
+
         // WAGE 테이블에서 cost 필드와 application의 earn 필드를 비교하여 동일한 경우
         if (wage.getCost() == application.getEarn()) {
             // application 객체의 Member의 restCash 필드에 application 객체의 earn 필드 값 전달
-            application.getMember().setRestCash(application.getEarn());
+            member.addRestCash(application.getEarn());
+            memberRepository.save(member);
             // application 객체의 earn 필드 값 0으로 변경
             application.setEarn(0);
             // application 객체의 receive 값을 true로 설정
             application.setReceive(true);
         }
-
         return application;
     }
 }
--- src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/ApplicationWriter.java
+++ src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/ApplicationWriter.java
@@ -18,14 +18,7 @@ public class ApplicationWriter implements ItemWriter<Application> {
 
     public void write(Chunk<? extends Application> items) throws Exception {
         for (Application application : items) {
-            System.out.println("------AFTER------");
-            System.out.println("Application.ID : " + application.getId());
-            System.out.println("Application.DEPOSIT : " + application.getDepositStatus());
-            System.out.println("Application.EARN : " + application.getEarn());
-            System.out.println("Application.JOB_COMPLETED : " + application.getJobCompleted());
-            System.out.println("Application.RECEIVE : " + application.isReceive());
-            System.out.println("------AFTER------");
-
+            System.out.println("------WRITER------");
             entityManager.merge(application); // 엔티티 저장
         }
     }
