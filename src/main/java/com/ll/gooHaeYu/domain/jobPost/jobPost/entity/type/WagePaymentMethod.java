d95ed1ef02e390875bb34962879368cc2961b54a
itonse <govl3dnjs@gmail.com>
2024-03-12 10:43:57 +0900

    feat: 급여 지급 방법 enum 클래스 추가

---
+++ src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/entity/type/WagePaymentMethod.java
@@ -0,0 +1,13 @@
+package com.ll.gooHaeYu.domain.jobPost.jobPost.entity.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum WagePaymentMethod {
+    INDIVIDUAL_PAYMENT("개인 지급"),
+    SERVICE_PAYMENT("서비스 결제");
+
+    private final String description;
+}
