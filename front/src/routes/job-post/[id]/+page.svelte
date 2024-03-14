6dd85af7ccb47d7dee2cb2a67abdbd449792eaca
itonse <govl3dnjs@gmail.com>
2024-03-14 19:20:51 +0900

    refactor: 공고 상세 - 조건에 기존 항목 몇개를 제거하고 작업 시간/일수 항목 추가

--- front/src/routes/job-post/[id]/+page.svelte
+++ front/src/routes/job-post/[id]/+page.svelte
@@ -301,31 +301,14 @@
 												d="M17.5,11c-3.584,0-6.5,2.916-6.5,6.5s2.916,6.5,6.5,6.5,6.5-2.916,6.5-6.5-2.916-6.5-6.5-6.5Zm0,12c-3.032,0-5.5-2.467-5.5-5.5s2.468-5.5,5.5-5.5,5.5,2.467,5.5,5.5-2.468,5.5-5.5,5.5Zm1.354-4.854c.195,.195,.195,.512,0,.707-.098,.098-.226,.146-.354,.146s-.256-.049-.354-.146l-1-1c-.094-.094-.146-.221-.146-.354v-2c0-.276,.224-.5,.5-.5s.5,.224,.5,.5v1.793l.854,.854Zm.646-14.146h-2.028c-.25-2.247-2.16-4-4.472-4h-2.5c-2.312,0-4.223,1.753-4.472,4h-1.528C2.019,4,0,6.019,0,8.5v11c0,2.481,2.019,4.5,4.5,4.5h6c.276,0,.5-.224,.5-.5s-.224-.5-.5-.5H4.5c-1.93,0-3.5-1.57-3.5-3.5v-6.5H9.5c.276,0,.5-.224,.5-.5s-.224-.5-.5-.5H1v-3.5c0-1.93,1.57-3.5,3.5-3.5h15c1.93,0,3.5,1.57,3.5,3.5v2c0,.276,.224,.5,.5,.5s.5-.224,.5-.5v-2c0-2.481-2.019-4.5-4.5-4.5ZM10.5,1h2.5c1.76,0,3.221,1.306,3.464,3H7.036c.243-1.694,1.704-3,3.464-3Z"
 											/></svg
 										>
-										<span class="font-medium">급여 타입:</span>
+										<span class="font-medium">작업 시간/일수:</span>
 										<span class="ml-2 text-gray-600">
-											{#if jobPostDetailDto?.payBasis === 'TOTAL_HOURS'}총 시간{:else if jobPostDetailDto?.payBasis === 'TOTAL_DAYS'}총
-												일수{:else if jobPostDetailDto?.payBasis === 'TOTAL_PROJECTS'}총 건수{:else if jobPostDetailDto?.payBasis === 'UNDEFINED'}미정{:else}'정보
-												없음'{/if}
+											{#if jobPostDetailDto?.payBasis === 'TOTAL_HOURS'}
+												{jobPostDetailDto.workTime}시간
+											{:else if jobPostDetailDto?.payBasis === 'TOTAL_DAYS'}
+												{jobPostDetailDto.workDays}일
+											{:else}정보 없음{/if}
 										</span>
-									</div>
-								</li>
-								<li class="flex items-center text-gray-700">
-									<div class="flex items-center">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											id="arrow-circle-down"
-											viewBox="0 0 24 24"
-											width="15"
-											height="15"
-											class="mr-2"
-											><path
-												d="M12,24C5.383,24,0,18.617,0,12S5.383,0,12,0s12,5.383,12,12-5.383,12-12,12ZM12,1C5.935,1,1,5.935,1,12s4.935,11,11,11,11-4.935,11-11S18.065,1,12,1Zm1.854,16.26l-4.877-4.876c-.211-.212-.211-.557,0-.768l4.873-4.872-.707-.707-4.873,4.873c-.601,.601-.601,1.58,0,2.181l4.877,4.877,.707-.707Z"
-											/></svg
-										>
-										<span class="font-medium">도움시간 또는 일수:</span>
-										<span class="ml-2 text-gray-600"
-											>{jobPostDetailDto?.workTime ?? '정보 없음'}</span
-										>
 									</div>
 								</li>
 								<li class="flex items-center text-gray-700">
