8930b884dd8baae1e61df4fef90b464e3961699b
itonse <govl3dnjs@gmail.com>
2024-03-14 15:25:10 +0900

    refactor: PayBasis 수정
    - 총 건수 제거

--- src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/entity/type/PayBasis.java
+++ src/main/java/com/ll/gooHaeYu/domain/jobPost/jobPost/entity/type/PayBasis.java
@@ -1,8 +1,7 @@
 package com.ll.gooHaeYu.domain.jobPost.jobPost.entity.type;
 
 public enum PayBasis {
-    UNDEFINED,   // 미정
+    UNDEFINED,   // 초기값
     TOTAL_HOURS,   // 총 시간
     TOTAL_DAYS,   // 총 일수
-    TOTAL_PROJECTS   // 총 건수
 }
