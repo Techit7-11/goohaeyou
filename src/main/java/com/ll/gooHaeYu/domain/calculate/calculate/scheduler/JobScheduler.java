9132670d363080ba948612ece4e2ba2b7b69a47c
jae9380 <145907325+jae9380@users.noreply.github.com>
2024-03-15 17:52:12 +0900

    Feature/chat (#135)
    * feat: displaying an info Message
    
    * feat: modifying info message
    
    * feat: Notification of creating a chat room
    
    * comment: 필요없는 주석 제거

--- src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/scheduler/JobScheduler.java
+++ src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/scheduler/JobScheduler.java
@@ -5,7 +5,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
