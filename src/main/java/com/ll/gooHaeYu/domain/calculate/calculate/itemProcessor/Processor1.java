d035d5c1f5ea10e9597ab492e279b833b1764dbe
jae9380 <ljy5314@gmail.com>
2024-03-15 10:35:32 +0900

    feat: automatic processing

--- src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/itemProcessor/Processor1.java
+++ src/main/java/com/ll/gooHaeYu/domain/calculate/calculate/itemProcessor/Processor1.java
@@ -4,13 +4,20 @@ import com.ll.gooHaeYu.domain.application.application.entity.Application;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import static com.ll.gooHaeYu.domain.application.application.entity.type.WageStatus.PAYMENT_COMPLETED;
+import static com.ll.gooHaeYu.domain.application.application.entity.type.WageStatus.SETTLEMENT_REQUESTED;
+
 @Component
 public class Processor1 implements ItemProcessor<Application, Application> {
 
     @Override
     public Application process(Application application) throws Exception {
 
-        System.out.println("------------------");
+        if (application.getWageStatus() == PAYMENT_COMPLETED) {
+            application.changeToCompleted();
+            application.updateWageStatus(SETTLEMENT_REQUESTED);
+        }
+
         return application;
     }
 }
