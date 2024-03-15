01c93ae07ea781d9fc24ee890d869e29eb529afc
itonse <govl3dnjs@gmail.com>
2024-03-15 23:16:13 +0900

    refactor: 캐시로그 서비스 리팩토링

--- src/main/java/com/ll/gooHaeYu/domain/payment/cashLog/service/CashLogService.java
+++ src/main/java/com/ll/gooHaeYu/domain/payment/cashLog/service/CashLogService.java
@@ -8,56 +8,20 @@ import com.ll.gooHaeYu.domain.payment.cashLog.repository.CashLogRepository;
 import com.ll.gooHaeYu.domain.payment.payment.dto.success.PaymentSuccessDto;
 import com.ll.gooHaeYu.domain.payment.payment.entity.Payment;
 import com.ll.gooHaeYu.domain.payment.payment.entity.type.PayStatus;
-import com.ll.gooHaeYu.domain.payment.payment.entity.type.PayTypeFee;
+import com.ll.gooHaeYu.domain.payment.payment.service.PaymentCalculationService;
 import com.ll.gooHaeYu.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-
-import static com.ll.gooHaeYu.global.exception.ErrorCode.NO_ENUM_CONSTANT_FOUND;
 import static com.ll.gooHaeYu.global.exception.ErrorCode.POST_NOT_EXIST;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-@Slf4j
 public class CashLogService {
+    private final PaymentCalculationService paymentCalculationService;
     private final CashLogRepository cashLogRepository;
-
-    // 토스페이먼츠 결제 부과세 10% 반환
-    public long getVat(long totalAmount) {
-        return (int) (totalAmount * 0.1);
-    }
-
-    // 결제 수수료 반환
-    public long getPaymentFee(PayStatus payStatus, long totalAmount) {
-        PayTypeFee payTypeFee = matchPayTypeFee(payStatus);
-
-        double feePercentage = payTypeFee.getFeePercentage();
-        int transactionFee = payTypeFee.getTransactionFee();
-
-        return (int) ((totalAmount * feePercentage / 100.0) + transactionFee);
-    }
-
-    private PayTypeFee matchPayTypeFee(PayStatus payStatus) {
-        return Arrays.stream(PayTypeFee.values())
-                .filter(payTypeFee -> payTypeFee.getTypeName().equals(payStatus.getDescription()))
-                .findFirst()
-                .orElseThrow(() -> new CustomException(NO_ENUM_CONSTANT_FOUND));   // 매칭되는 PayTypeFee가 없는 경우
-    }
-
-    // 부가세와 결제 수수료의 합 반환
-    public long getTotalTaxAndFees(PayStatus payStatus, long totalAmount) {
-        return getVat(totalAmount) + getPaymentFee(payStatus, totalAmount);
-    }
-
-    // 순금액 반환
-    public long getNetAmount (PayStatus payStatus, long totalAmount) {
-        return totalAmount - getTotalTaxAndFees(payStatus, totalAmount);
-    }
 
     public CashLogDto findByApplicationId(Long id) {
         CashLog cashLog = findByApplicationIdAndValidate(id);
@@ -70,21 +34,21 @@ public class CashLogService {
                 .orElseThrow(() -> new CustomException(POST_NOT_EXIST));
     }
 
-    @Transactional
-    public void addCashLog(CashLog cashLog) {
+    private void addCashLog(CashLog cashLog) {
         cashLogRepository.save(cashLog);
     }
 
-    public void createCashLog(Application application) {
+    @Transactional
+    public void createCashLogOnSettled(Application application) {
         long earn = application.getEarn();
         CashLog newCashLog =  CashLog.builder()
                 .member(application.getMember())
                 .description("지원서_" + application.getId() + "_대금_결제")
                 .eventType(EventType.정산_급여)
                 .totalAmount(earn)
-                .vat(getVat(earn))
-                .paymentFee(getPaymentFee(PayStatus.EASY_PAY,earn))
-                .netAmount(getNetAmount(PayStatus.EASY_PAY,earn))
+                .vat(paymentCalculationService.getVat(earn))
+                .paymentFee(paymentCalculationService.getPaymentFee(PayStatus.EASY_PAY,earn))
+                .netAmount(paymentCalculationService.getNetAmount(PayStatus.EASY_PAY,earn))
                 .applicationId(application.getId())
                 .build();
 
@@ -100,15 +64,16 @@ public class CashLogService {
                 .description(successDto.getOrderName())
                 .eventType(EventType.결제_토스페이먼츠)
                 .totalAmount(successDto.getTotalAmount())
-                .vat(getVat(payment.getTotalAmount()))
-                .paymentFee(getPaymentFee(payStatus, payment.getTotalAmount()))
-                .netAmount(getNetAmount(payStatus, payment.getTotalAmount()))
+                .vat(paymentCalculationService.getVat(payment.getTotalAmount()))
+                .paymentFee(paymentCalculationService.getPaymentFee(payStatus, payment.getTotalAmount()))
+                .netAmount(paymentCalculationService.getNetAmount(payStatus, payment.getTotalAmount()))
                 .applicationId(payment.getApplicationId())
                 .build();
 
         addCashLog(newCashLog);
     }
 
+    @Transactional
     public void createCashLogOnCancel(Payment payment) {
         CashLog newCashLog = CashLog.builder()
                 .member(payment.getMember())
