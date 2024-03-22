a29cb1587cd7480ac6332d2fa6518f0a6c8f0d8b
itonse <govl3dnjs@gmail.com>
2024-03-22 23:19:00 +0900

    test: 지원서 작성 실패 테스트 완료

--- src/test/java/com/ll/gooHaeYu/controller/application/ApplicationControllerIntegrationTest.java
+++ src/test/java/com/ll/gooHaeYu/controller/application/ApplicationControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.ll.gooHaeYu.controller.application;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.gooHaeYu.domain.application.application.dto.ApplicationForm;
 import com.ll.gooHaeYu.domain.jobPost.jobPost.dto.JobPostDto;
 import com.ll.gooHaeYu.domain.jobPost.jobPost.service.JobPostService;
@@ -24,10 +23,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Duration;
 import java.util.List;
 
+import static com.ll.gooHaeYu.global.config.AppConfig.objectMapper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,9 +36,6 @@ public class ApplicationControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Autowired
     private MemberService memberService;
@@ -53,7 +49,7 @@ public class ApplicationControllerIntegrationTest {
     private static final Duration ACCESS_TOKEN_DURATION = Duration.ofMinutes(30);
     private static final String ACCESS_TOKEN_COOKIE_NAME = "access_token";
 
-    @DisplayName("지원서 작성 - 성공")
+    @DisplayName("지원서 작성 성공")
     @Test
     void writeApplicationSuccess() throws Exception {
         // Given
@@ -62,9 +58,7 @@ public class ApplicationControllerIntegrationTest {
 
         List<JobPostDto> jobPostDtos = jobPostService.findByUsername("recruiter");
 
-        memberService.join(new JoinFormBuilder()
-                .setUsername("applicant")
-                .build());
+        memberService.join(new JoinFormBuilder().setUsername("applicant").build());
 
         ApplicationForm.Register form = writeApplicationForm();
 
@@ -72,16 +66,42 @@ public class ApplicationControllerIntegrationTest {
 
         String accessToken = jwtTokenProvider.generateToken(member, ACCESS_TOKEN_DURATION);
 
-        // When & Then
+        // When
         ResultActions resultActions = mockMvc.perform(post("/api/applications/{id}", jobPostDtos.get(0).getId())
                         .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(form)))
                 .andDo(print());
 
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.statusCode").value(201))
+        // Then
+        resultActions.andExpect(jsonPath("$.statusCode").value(201))
                 .andExpect(jsonPath("$.message").value("Created"))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("지원서 작성 실패 - 존재하지 않는 공고에 지원")
+    @Test
+    void writeApplicationFail() throws Exception {
+        // Given
+        memberService.join(new JoinFormBuilder().setUsername("applicant").build());
+
+        ApplicationForm.Register form = writeApplicationForm();
+
+        Member member = memberService.getMember("applicant");
+
+        String accessToken = jwtTokenProvider.generateToken(member, ACCESS_TOKEN_DURATION);
+
+        // When
+        ResultActions resultActions = mockMvc.perform(post("/api/applications/{id}", 10)
+                        .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andDo(print());
+
+        // Then
+        resultActions.andExpect(jsonPath("$.statusCode").value(404))
+                .andExpect(jsonPath("$.resultType").value("CUSTOM_EXCEPTION"))
+                .andExpect(jsonPath("$.message").value("해당 공고는 존재하지 않습니다."))
                 .andExpect(jsonPath("$.data").isEmpty());
     }
 
