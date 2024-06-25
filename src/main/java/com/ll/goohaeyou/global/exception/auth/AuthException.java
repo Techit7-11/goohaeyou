4f4a434dfe202408464f2d18333fcb929e74797a
itonse <govl3dnjs@gmail.com>
2024-06-26 04:00:00 +0900

    refactor: 예외를 도메인 별로 분리해서 클래스 생성

---
+++ src/main/java/com/ll/goohaeyou/global/exception/auth/AuthException.java
@@ -0,0 +1,34 @@
+package com.ll.goohaeyou.global.exception.auth;
+
+import com.ll.goohaeyou.global.exception.ErrorCode;
+import com.ll.goohaeyou.global.exception.GoohaeyouException;
+
+import static com.ll.goohaeyou.global.exception.ErrorCode.*;
+
+public class AuthException extends GoohaeyouException {
+
+    public AuthException(final ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public static class InvalidPasswordException extends AuthException {
+
+        public InvalidPasswordException() {
+            super(INVALID_PASSWORD);
+        }
+    }
+
+    public static class NotAuthorizedException extends AuthException {
+
+        public NotAuthorizedException() {
+            super(NOT_AUTHORIZED);
+        }
+    }
+
+    public static class InvalidLoginRequestException extends AuthException {
+
+        public InvalidLoginRequestException() {
+            super(INVALID_LOGIN_REQUEST);
+        }
+    }
+}
