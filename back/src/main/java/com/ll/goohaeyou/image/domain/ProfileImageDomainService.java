4de52ab408fe00c07c04368eb1ed0cc2824ecade
itonse <govl3dnjs@gmail.com>
2024-09-13 08:02:49 +0900

    refactor: ProfileImageDomainService를 비즈니스 서비스와 도메인 서비스로 분리

---
+++ back/src/main/java/com/ll/goohaeyou/image/domain/ProfileImageDomainService.java
@@ -0,0 +1,27 @@
+package com.ll.goohaeyou.image.domain;
+
+import com.ll.goohaeyou.global.standard.anotations.DomainService;
+import com.ll.goohaeyou.jobApplication.domain.ImageStorage;
+import com.ll.goohaeyou.member.member.domain.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@DomainService
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProfileImageDomainService {
+    private final ImageStorage imageStorage;
+
+    @Transactional
+    public void upload(Member member, MultipartFile profileImageFile) {
+        String imageUrl = imageStorage.upload(profileImageFile);
+        member.updateImageUrl(imageUrl);
+    }
+
+    @Transactional
+    public void delete(Member member) {   // S3에서 이미지 제거, DB에서 이미지 url 제거
+        imageStorage.deleteImageFromS3(member.getProfileImageUrl());
+        member.updateImageUrl(null);
+    }
+}
