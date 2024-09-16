06794dab46c096d14719d010b279aeb9ec266f1a
itonse <govl3dnjs@gmail.com>
2024-09-17 04:58:38 +0900

    refactor: 낙관적 락 설정 수정
    - 대상 메서드, backoff 수정

--- back/src/main/java/com/ll/goohaeyou/payment/payment/application/PaymentService.java
+++ back/src/main/java/com/ll/goohaeyou/payment/payment/application/PaymentService.java
@@ -28,7 +28,6 @@ public class PaymentService {
     private final PaymentDomainService paymentDomainService;
 
     @Transactional
-    @RetryOnOptimisticLock(attempts = 2, backoff = 500L)
     public PaymentResponse requestTossPayment(PaymentRequest request, String username) {
         Member member = memberDomainService.getByUsername(username);
 
@@ -37,6 +36,7 @@ public class PaymentService {
         return paymentDomainService.preparePaymentResponse(payment);
     }
 
+    @RetryOnOptimisticLock
     @Transactional
     public PaymentSuccessResponse tossPaymentSuccess(String paymentKey, String orderId, Long amount) {
         Payment payment = paymentDomainService.getByOrderId(orderId);
